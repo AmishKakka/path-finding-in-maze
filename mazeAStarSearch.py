438fed826d5b007db7dd593fb98888ff8a589bc7
AmK-inventor-0134 <76550625+AmishKakka@users.noreply.github.com>
2022-11-07 16:12:22 +0530

    Add files via upload

---
+++ mazeAStarSearch.py
@@ -0,0 +1,116 @@
+import maze
+from queue import PriorityQueue
+
+class Node():
+    def __init__(self, state, parent, action):
+        self.state = state
+        self.parent = parent
+        self.action = action
+
+class StackFrontier():
+    def __init__(self):
+        self.frontier = []
+
+    def add(self, node):
+        self.frontier.append(node)
+
+    def contains_state(self, state):
+        return any(node.state == state for node in self.frontier)
+
+    def empty(self):
+        return len(self.frontier) == 0
+
+    def remove(self):
+        if self.empty():
+            raise Exception("empty frontier")
+        else:
+            node = self.frontier[-1]
+            self.frontier = self.frontier[:-1]
+            return node
+
+
+class QueueFrontier(StackFrontier):
+    def remove(self):
+        if self.empty():
+            raise Exception("empty frontier")
+        else:
+            node = self.frontier[0]
+            self.frontier = self.frontier[1:]
+            return node
+
+
+class AStar():
+    def __init__(self, file):
+        # Initialize an empty explored set
+        self.explored = set()
+
+        self.m = maze.Maze(file)
+        print("Solving using A* Search.")
+        """Finds a solution to maze, if one exists."""
+
+        # Keep track of number of states explored
+        self.num_explored = 0
+
+        # Initialize frontier to just the starting position
+        self.start = Node(state=self.m.start, parent=None, action=None)
+        self.frontier = QueueFrontier()
+        self.frontier.add(self.start)
+
+    def heuristic(self, d1, d2, d3):
+        d1_d2 = (abs(d1[0]-d2[0]) + abs(d1[1]-d2[1]))
+        d2_d3 = (abs(d3[0]-d2[0]) + abs(d3[1]-d2[1]))
+        return d1_d2+d2_d3
+
+    def solve(self):
+        # Keep looping until solution found
+        while True:
+            # If nothing left in frontier, then no path
+            if self.frontier.empty():
+                raise Exception("no solution")
+
+            # Choose a node from the frontier
+            node = self.frontier.remove()
+            self.num_explored += 1
+
+            # If node is the goal, then we have a solution
+            if node.state == self.m.goal:
+                print("Reached goal state...\n")
+                actions = []
+                cells = []
+                while node.parent is not None:
+                    actions.append(node.action)
+                    cells.append(node.state)
+                    node = node.parent
+                actions.reverse()
+                cells.reverse()
+                self.m.solution = (actions, cells)
+                self.m.explored = self.explored
+
+                #print("\nStates explored: ", self.num_explored)
+                #print("Found Solution.")
+                self.sol = self.m.output_image(filename='Path Finding in Maze\mazeSolution.png', 
+                                                show_solution=True,
+                                                show_explored=True)
+                #print("\nCreated Solution image.")
+                return
+
+            # Mark node as explored
+            self.explored.add(node.state)
+            # Creating a list which will hold the distances along with (action, state)
+            distances = []
+        
+            # Add neighbors to frontier
+            for action, state in self.m.neighbors(node.state):
+                if not self.frontier.contains_state(state) and state not in self.explored:
+                    distances.append((self.heuristic(self.start.state, state, self.m.goal), (action, state)))
+            
+            if distances is None:
+                print(state, "\n")
+                # Doing this we go to the next-best state with the same parent node but different heuristic value.
+                continue
+            else:
+                distances = sorted(distances)
+                for d in distances:
+                    child = Node(state=d[1][1], parent=node, action=d[1][0])
+                    self.frontier.add(child)
+            del distances
---
+++ window.py
@@ -0,0 +1,104 @@
+import tkinter as tk
+from tkinter.filedialog import askopenfilename
+from PIL import ImageTk, Image
+import maze
+
+
+class MainWindow():
+    def __init__(self):
+        self.base = tk.Tk()
+        self.base.title('Path finding in maze')
+        self.width, self.height = 300, 200
+        #self.base.iconphoto(False, tk.PhotoImage(file='..\maze.png'))
+        self.base.geometry(str(self.width)+'x'+str(self.height))
+        self.base.minsize(self.width, self.height)
+        self.base.maxsize(self.base.winfo_screenwidth(), self.base.winfo_screenheight())
+
+        self.upload = tk.Button(self.base, text="Upload .txt file which has the maze", 
+                                bg='yellow',
+                                bd=1.5,        
+                                font='Times 15',
+                                relief='ridge',
+                                command=self.open_file)
+        self.upload.pack(side='top', pady=15)
+
+        self.options = []
+        self.base.mainloop()
+
+    def open_file(self):
+        self.file = askopenfilename(filetypes=(("Text Files", "*.txt"),))
+        try:
+            if self.file is not None:
+                # Calling the maze module which has the Maze class.
+                self.m = maze.Maze(self.file)
+                self.m.output_image("Path Finding in Maze/maze.png")
+                self.image = Image.open("Path Finding in Maze/maze.png")
+                
+                # Resizing the image if it is beyond the screen limits.
+                if self.image.size[0] >= self.base.winfo_screenwidth()-100:
+                    self.image = self.image.resize((self.base.winfo_screenwidth()-100, self.image.size[1]))
+                if self.image.size[1] >= self.base.winfo_screenheight()-200:
+                    self.image = self.image.resize((self.image.size[0], self.base.winfo_screenheight()-200))
+                
+                # Holding on to the image so that no garbage collector error shows up.
+                self.img = ImageTk.PhotoImage(image=self.image)
+                self.width, self.height = int(self.image.size[0]*1.5), int(self.image.size[1]*1.75)
+                self.base.geometry(str(self.width)+'x'+str(self.height))
+            
+                self.canvas = tk.Canvas(self.base, height=self.image.size[1], width=self.image.size[0])
+                self.canvas.pack(side='top', pady=5)
+                self.canvas.create_image(self.image.size[0]//2, self.image.size[1]//2, image=self.img)
+                
+                self.showSol = tk.Button(self.base, text="Show Solution",
+                                        bg='yellow',
+                                        bd=1.5,
+                                        font='Times 20',
+                                        relief='ridge',
+                                        command=self.solve_maze)
+                self.showSol.pack(side='bottom', pady=15)
+        except FileNotFoundError as fe:
+            print("Error: Please upload a valid file !")
+
+    def solve_maze(self):
+        import mazeAStarSearch
+        self.aStar = mazeAStarSearch.AStar(self.file)
+        # Calling the 'solve' function inside the aStar class.
+        self.aStar.solve()
+        self.numStates, self.statesExplored = self.aStar.num_explored-1, self.aStar.sol
+        
+        img_sol = Image.open("Path Finding in Maze/mazeSolution.png")
+        self.sol_img = ImageTk.PhotoImage(image=img_sol)
+        self.canvas.create_image(self.image.size[0]//2, self.image.size[1]//2, image=self.sol_img)
+        self.showSol.destroy()
+        self.moreOptions = tk.Button(self.base, text="More Information",
+                                        bg='blue',
+                                        bd=1.5,
+                                        font='Times 20',
+                                        relief='ridge',
+                                        command=self.newWindow)
+        self.moreOptions.pack(side='bottom', pady=5)
+
+    def newWindow(self):
+        self.nW = tk.Toplevel(self.base, background='cyan')
+        self.nW.geometry('400x200')
+        self.nW.title('Details')
+        
+        label = tk.Label(self.nW, text="Explored a total of "+str(self.numStates)+" states.", background='cyan')
+        label.config(font=('Times New Roman', 15))
+        label.pack(side='top', pady=5)
+
+        label2 = tk.Label(self.nW, text="Solution states\nROW          COLUMN", height=2, background='cyan')
+        label2.config(font=('Times New Roman', 12))
+        label2.pack(side='top', pady=0)
+
+        label3 = tk.Label(self.nW, background='cyan')
+        label3.pack(side='top', pady=0)
+        scroll_bar = tk.Scrollbar(label3, orient=tk.VERTICAL, width=0)
+        scroll_bar.pack(side='right')
+        mylist = tk.Listbox(label3, yscrollcommand=scroll_bar.set)
+        for idx in self.statesExplored:
+            mylist.insert(tk.END, "         "+str(idx[0])+"       ||         "+str(idx[1]))
+        mylist.pack(side='top')
+        scroll_bar.config(command=mylist.yview) 
+    
+
